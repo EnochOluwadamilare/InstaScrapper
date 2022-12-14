f23a58db312043416cad822d777e8a38a9cfa07b
cherrio <cherrio.llc@gmail.com>
2022-12-14 22:13:30 +0100

    Initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 group = "com.cherrio"
 version = "0.0.1"
 application {
-    mainClass.set("io.ktor.server.netty.EngineMain")
+    mainClass.set("com.cherrio.ApplicationKt")
 
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
--- src/main/kotlin/com/cherrio/Application.kt
+++ src/main/kotlin/com/cherrio/Application.kt
@@ -3,12 +3,12 @@ package com.cherrio
 import com.cherrio.instagram.scrapperRouting
 import io.ktor.server.application.*
 import com.cherrio.plugins.*
+import io.ktor.server.netty.*
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.time.delay
 import java.time.Duration
 
-fun main(args: Array<String>): Unit =
-        io.ktor.server.netty.EngineMain.main(args)
+fun main(args: Array<String>): Unit = EngineMain.main(args)
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
