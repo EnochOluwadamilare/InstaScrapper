c544ea0ccb5738a76d37272e2ba5a67d593d834d
cherrio <cherrio.llc@gmail.com>
2022-12-20 10:14:08 +0100

    AbeokutaVendors 4

--- src/main/kotlin/com/cherrio/Application.kt
+++ src/main/kotlin/com/cherrio/Application.kt
@@ -3,15 +3,21 @@ package com.cherrio
 import com.cherrio.instagram.scrapperRouting
 import io.ktor.server.application.*
 import com.cherrio.plugins.*
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.netty.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.time.delay
+import kotlinx.serialization.json.Json
 import java.time.Duration
 
 fun main(args: Array<String>): Unit = EngineMain.main(args)
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
+    configureSerialization()
     configureRouting()
     configureLogging()
     scrapperRouting()
@@ -24,3 +30,18 @@ fun Application.module() {
 
 }
 
+fun Application.configureSerialization() {
+    install(ContentNegotiation) {
+        json(Json {
+            ignoreUnknownKeys = true
+            isLenient = true
+            prettyPrint = true
+        })
+    }
+
+    routing {
+        get("/json/kotlinx-serialization") {
+            call.respond(mapOf("hello" to "world"))
+        }
+    }
+}
--- src/main/kotlin/com/cherrio/plugins/Routing.kt
+++ src/main/kotlin/com/cherrio/plugins/Routing.kt
@@ -1,12 +1,16 @@
 package com.cherrio.plugins
 
+import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.server.routing.*
 import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
 import io.ktor.server.response.*
 import io.ktor.server.request.*
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
 import java.io.File
 
 fun Application.configureRouting() {
