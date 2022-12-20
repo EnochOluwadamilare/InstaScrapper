4b2c655b2f0d5f6b6c6738197094bcc4ca28cf04
cherrio <cherrio.llc@gmail.com>
2022-12-20 10:08:41 +0100

    AbeokutaVendors 3

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,9 @@ repositories {
 dependencies {
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
+
     implementation("ch.qos.logback:logback-classic:$logback_version")
 
     implementation("io.ktor:ktor-client-core:$ktor_version")
